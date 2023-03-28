f53a237327e88d6d2afc8510980b0cf7ac6b4a38
Amina Ferraoun <35732195+AminaFerraoun@users.noreply.github.com>
2023-03-29 00:29:40 +0200

    Configuration ok

---
+++ ws_archisi/BookManagerEJB/ejbModule/fr/univtours/polytech/bookmanager/model/BookBean.java
@@ -0,0 +1,31 @@
+package fr.univtours.polytech.bookmanager.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "BOOKS")
+public class BookBean implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Integer idBook;
+	private String BookTitle;
+	
+	public String getBookTitle() {
+		return BookTitle;
+	}
+	public void setBookTitle(String bookTitle) {
+		BookTitle = bookTitle;
+	}
+	
+}
