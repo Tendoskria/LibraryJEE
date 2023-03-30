07631b20c1ef3cc9b4bd35c2cbdc56594f5015bd
Amina Ferraoun <35732195+AminaFerraoun@users.noreply.github.com>
2023-03-30 09:07:45 +0200

    test de jsp

--- ws_archisi/BookManagerEJB/ejbModule/fr/univtours/polytech/bookmanager/model/BookBean.java
+++ ws_archisi/BookManagerEJB/ejbModule/fr/univtours/polytech/bookmanager/model/BookBean.java
@@ -6,11 +6,16 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "BOOK")
 public class BookBean implements Serializable {
+
+
+	
 
 	/**
 	 * 
--- ws_archisi/BookManagerEJB/ejbModule/fr/univtours/polytech/bookmanager/model/GenreBean.java
+++ ws_archisi/BookManagerEJB/ejbModule/fr/univtours/polytech/bookmanager/model/GenreBean.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -13,6 +14,7 @@ import javax.persistence.Table;
 public class GenreBean implements Serializable {
 
 	
+
 	/**
 	 * 
 	 */
